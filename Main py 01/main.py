d9821c7c61bfb6175125f0a8b562a16355ba10f3
kperryman <101473071+kperryman@users.noreply.github.com>
2022-03-15 11:33:24 -0700

    Add files via upload

---
+++ Main py 01/main.py
@@ -0,0 +1,19 @@
+print("hello World")
+print("This is a Python program")
+print("What should we do next?")
+x = 5
+y="John"
+print (x)
+y=int(3)
+z=float(3)
+print (x)
+print (y)
+print (z)
+print (x+z)
+a=2
+b=18
+print(b-a)
+print(b/a)
+print (a*b)
+print (a%b)
+print (a/b)
